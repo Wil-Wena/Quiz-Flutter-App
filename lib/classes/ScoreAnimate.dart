6cc25aba17b229c755371001e996b98d5c2aeefb
Wilson Aballey <willywallet001@gmail.com>
2022-06-24 18:06:47 +0000

    sixth commit

---
+++ lib/classes/ScoreAnimate.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get_navigation/src/routes/default_transitions.dart';
+
+class ScoreAnimation extends StatefulWidget {
+  const ScoreAnimation({Key? key}) : super(key: key);
+
+  @override
+  State<ScoreAnimation> createState() => _ScoreAnimationState();
+}
+
+class _ScoreAnimationState extends State<ScoreAnimation>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: Duration(seconds: 3),
+  )..repeat(reverse: false);
+  late final Animation<Offset> _animation = Tween(
+          begin: Offset.fromDirection(1.50), end: Offset.fromDirection(-1.50))
+      .animate(
+          CurvedAnimation(parent: _controller, curve: Curves.easeInOutCubic));
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _animation,
+      child: Image.asset(
+        'assets/pngwing.com (8).png',
+        height: 1000,
+        width: 5000,
+      ),
+    );
+  }
+}
